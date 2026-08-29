61d2e5da873012f3dc0c9c175ec6b133c0aa806d
ccyberpetya <petya.yudin2@gmail.com>
2026-08-29 12:14:35 +0500

    subnet

--- terraform/infrastructure/variables.tf
+++ terraform/infrastructure/variables.tf
@@ -13,3 +13,28 @@ variable "network_name" {
   type        = string
   default     = "devops-dip-network"
 }
+variable "subnets" {
+  description = "VPC subnets for Kubernetes nodes"
+
+  type = map(object({
+    zone = string
+    cidr = string
+  }))
+
+  default = {
+    a = {
+      zone = "ru-central1-a"
+      cidr = "10.10.1.0/24"
+    }
+
+    b = {
+      zone = "ru-central1-b"
+      cidr = "10.10.2.0/24"
+    }
+
+    d = {
+      zone = "ru-central1-d"
+      cidr = "10.10.3.0/24"
+    }
+  }
+}
